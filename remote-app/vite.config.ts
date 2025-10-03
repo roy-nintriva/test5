d0d88d605f2d87c435e23156bbdbb4c3a532baa4
roy-nintriva <roy.pa@zcomsystems.com>
2025-10-03 14:20:51 +0530

    fix: some changes

--- remote-app/vite.config.ts
+++ remote-app/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {federation} from '@module-federation/vite'
+import { federation } from '@module-federation/vite'
 
 export default defineConfig({
   base: 'http://localhost:4174/',
@@ -13,10 +13,10 @@ export default defineConfig({
         './Button': './src/Button.tsx',
       },
       shared: {
-  react: { "singleton": true, "requiredVersion": "^19.1.0" },
-  "react-dom": { "singleton": true, "requiredVersion": "^19.1.0" },
-  
-}
+        react: { "singleton": true, "requiredVersion": "^19.1.0" },
+        "react-dom": { "singleton": true, "requiredVersion": "^19.1.0" },
+
+      }
     }),
   ],
   server: {
