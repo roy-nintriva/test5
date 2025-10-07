174be4691e26b103be4dd8ed5b568906091a85cf
roy-nintriva <roy.pa@zcomsystems.com>
2025-10-07 19:43:09 +0530

    fix: some changes

--- host-app/src/main.tsx
+++ host-app/src/main.tsx
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// // import { BrowserRouter } from 'react-router-dom';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-// const container = document.getElementById('root');
-// const root = ReactDOM.createRoot(container!);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container!);
 
-// root.render(
-//   <React.StrictMode>
-//     {/* <BrowserRouter> */}
-//       <App />
-//     {/* </BrowserRouter> */}
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+    {/* <BrowserRouter> */}
+      <App />
+    {/* </BrowserRouter> */}
+  </React.StrictMode>
+);
 
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
--- host-app/vite.config.ts
+++ host-app/vite.config.ts
@@ -1,36 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { federation } from '@module-federation/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: 'host_app',
+      name: "host_app",
       remotes: {
-        remote_app: {
-          type: 'module',
-          name: 'remote_app',
-          entry: 'http://localhost:4174/remoteEntry.js',
-        },
+        entry: "http://localhost:4174/remoteEntry.js",
       },
-shared: {
-  react: { "singleton": true, "requiredVersion": "^19.1.0" },
-  "react-dom": { "singleton": true, "requiredVersion": "^19.1.0" },
-  
-}
-
+      shared: ["react", "react-dom"],
     }),
   ],
   server: {
     port: 5174,
-    origin: 'http://localhost:5174',
+    origin: "http://localhost:5174",
     strictPort: true,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
-})
-
-
-
+});
